c5a9b66e9895fc818fb43734d9d237d64fe7c120
Walter Oesch <walter.oesch@outlook.com>
2017-05-12 00:35:43 +0200

    Db Klasse

--- src/ch/oesch/Db.java
+++ src/ch/oesch/Db.java
@@ -9,16 +9,18 @@ public class Db {
     private static final String oracleThinConnectionString = "jdbc:oracle:thin:@localhost:1521:xe";
     private static final String sqlServerEpressConnectionString2 = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=abc;integratedSecurity=true";
     private static final String sqlServerEpressConnectionString = "jdbc:sqlserver://" + "localhost;" + "instance=SQLEXPRESS;databaseName=walter_test;user=walter;password=1234";
-    public static final String mysqlConnectionString = "jdbc:mysql://localhost/test?";
+    private static final String mysqlConnectionString = "jdbc:mysql://localhost/test?";
 
-    public static String connectionString = "jdbc:oracle:thin:@localhost:1521:xe";
+    public static String connectionString = null;
     public static String username;
     public static String password;
 
     public static ResultSet execute(String query){
         ResultSet rs = null;
+        if(connectionString == null) {
+            return rs;
+        }
         try {
-
             Connection con = DriverManager.getConnection(
                     connectionString,
                     username,
